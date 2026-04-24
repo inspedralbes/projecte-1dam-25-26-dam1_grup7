cfe9488b70289a8199bf44cb4d13da1c7f9b775c
a25unaalvlop <a25unaalvlop@inspedralbes.cat>
2026-04-24 10:46:46 +0200

    Creació pàgina usuaris

---
+++ Docker_GI3P/php/usuaris.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administracio-Usuari</title>
+</head>
+<body>
+    <header>
+        <h1 style="text-align: center;">Gestor d'incidencies per a Usuaris</h1>
+    </header>
+    <nav style="text-align: center;">
+        <a href="crear_incidencia.php"><input type="button" value="Crear Incidencies"></a>
+        <a href="veure_incidencia.php"><input type="button" value="Comporbar Incidencies"></a>
+        <a href="consum_departaments.php"><input type="button" value="Comprobar consum de departaments"></a>
+    </nav>
+</body>
+</html>
