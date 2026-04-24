8b83b40561003a7378ac33609433b1be13903a42
a25sercanper <a25sercanper@inspedralbes.cat>
2026-04-24 10:22:15 +0200

    Creació de header.php i modificació de colors en el style

--- Docker_GI3P/php/index.php
+++ Docker_GI3P/php/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="ca">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Gestor d'Incidències</title>
-</head>
+<?php include_once "header.php"; ?>
 
 <body>
     <header>
@@ -16,13 +8,11 @@
     <main>
         <div id="menu">
         <hr>
-        <a href="usuaris.php"><input type="button" value="Usuaris"></a>
-        <a href="tecnics.php"><input type="button" value="Tècnics"></a>
-        <a href="admin.php"><input type="button" value="Administradors"></a>
+        <a href="usuaris.php"><input id="user-btn" type="button" value="Usuaris"></a>
+        <a href="tecnics.php"><input id="tech-btn" type="button" value="Tècnics"></a>
+        <a href="admin.php"><input id="admin-btn" type="button" value="Administradors"></a>
     </div>
     </main>
-
-    
 
 <footer>
     <p>Projecte Final Unai i Sergi</p>
