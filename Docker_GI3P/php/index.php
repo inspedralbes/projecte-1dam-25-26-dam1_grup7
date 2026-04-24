8d87d13220dedee8292b6a29be3bdc0071a0c11c
a25sercanper <a25sercanper@inspedralbes.cat>
2026-04-24 11:34:43 +0200

    index.html eliminat i modificació dels estils

--- Docker_GI3P/php/index.php
+++ Docker_GI3P/php/index.php
@@ -1,21 +1,22 @@
-<?php include_once "header.php"; ?>
+<?php include_once "header.php";?>
 
 <body>
     <header>
         <h1>Gestor d'incidències</h1>
     </header>
-
+    <hr>
     <main>
         <div class="menu">
-        <hr>
+        
         <a href="usuaris.php"><input id="user-btn" type="button" value="Usuaris"></a>
         <a href="tecnics.php"><input id="tech-btn" type="button" value="Tècnics"></a>
         <a href="admin.php"><input id="admin-btn" type="button" value="Administradors"></a>
     </div>
     </main>
-
+    <hr>
 <footer>
-    <p>Projecte Final Unai i Sergi</p>
+    <p>Projecte Final</p>
+    <p>Unai Álvarez | Sergi Cantallops</p>
 </footer>
 
 </body>
