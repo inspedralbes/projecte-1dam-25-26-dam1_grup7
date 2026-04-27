9ba8874ccde495c6f3280343d8791712c3ec1ee9
a25unaalvlop <a25unaalvlop@inspedralbes.cat>
2026-04-27 12:40:24 +0200

    Pàgines principals creades

---
+++ Docker_GI3P/php/admin.php
@@ -0,0 +1,13 @@
+<?php include_once "header.php";?>
+<body class="page-admin">
+    <header>
+        <h1>Gestor d'incidències per a Administradors</h1>
+    </header>
+    <main>
+        <div class="menu">
+            <a href="informacio_tecnics.php"><input type="button" class="btn-inf-tecn" value="Informació de Tècnics"></a>
+            <a href="assignar_incidencia.php"><input type="button" class="btn-assign-inc" value="Assignar Incidències"></a>
+            <a href="estadistiques_acces.php"><input type="button" class="btn-est-accs" value="Estadistiques d'Accés"></a>
+        </div>
+    </main>
+</body>
--- Docker_GI3P/php/usuaris.php
+++ Docker_GI3P/php/usuaris.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administracio-Usuari</title>
-</head>
-<body>
+<?php include_once "header.php";?>
+<body class="page-users">
     <header>
-        <h1 style="text-align: center;">Gestor d'incidencies per a Usuaris</h1>
+        <h1>Gestor d'incidències per a Usuaris</h1>
     </header>
-    <nav style="text-align: center;">
-        <a href="crear_incidencia.php"><input type="button" value="Crear Incidencies"></a>
-        <a href="veure_incidencia.php"><input type="button" value="Comporbar Incidencies"></a>
-        <a href="consum_departaments.php"><input type="button" value="Comprobar consum de departaments"></a>
-    </nav>
+    <main>
+        <div class="menu">
+            <a href="crear_incidencia.php"><input type="button" class="btn-crear" value="Crear Incidències"></a>
+            <a href="veure_incidencia.php"><input type="button" class="btn-consultar-inc" value="Comprovar Incidències"></a>
+            <a href="consum_departaments.php"><input type="button" class="btn-consultar-dep" value="Consum Departaments"></a>
+        </div>
+    </main>
 </body>
-</html>
