21915ebdb7eeccff96c63455c7089dbdda773037
a25unaalvlop <a25unaalvlop@inspedralbes.cat>
2026-04-28 12:30:37 +0200

    Incidencies i actuacions finalitzat

--- Docker_GI3P/php/veure_actuacions.php
+++ Docker_GI3P/php/veure_actuacions.php
@@ -1,9 +1,16 @@
 <?php include_once "header.php"; ?>
 <?php require_once 'connexio.php'; ?>
 
+<?php
+
+if (isset($_GET['id'])) {
+    $id_incidencia = $_GET['id'];
+}
+    ?>
+
 <body>
-    <h1>Consultar Incidencia</h1>
-    <?php $sql = "SELECT * FROM incidencia";
+    <h1>Actuacions</h1>
+    <?php $sql = "SELECT * FROM incidencia where id = $id_incidencia";
     $result = $conn->query($sql); ?>
     <table>
         <tr style="border: 1px solid black;">
@@ -53,11 +60,48 @@
         }
         ?>
     </table>
-    <?php
-    function crear_incidencia($conn) {
+    <hr>
+    <?php $sql = "SELECT * FROM actuacions where incidencia = $id_incidencia";
+    $result = $conn->query($sql); ?>
 
-    }
+     <table>
+        <tr style="border: 1px solid black;">
+            <th style="border: 1px solid black;">
+                ID Actuacio
+            </th>
+            <th style="border: 1px solid black;">
+                Data Actuacio
+            </th>
+            <th style="border: 1px solid black;">
+                Descripció
+            </th>
+            <th style="border: 1px solid black;">
+                Visible
+            </th>
+            <th style="border: 1px solid black;">
+                Temps total
+            </th>
+            <th style="border: 1px solid black;">
+                ID
+            </th>
+        </tr>
+        <?php
+       while ($row = $result->fetch_assoc()) {
+        echo "<tr style='border: 1px solid black;'>";
+        echo "<td style='border: 1px solid black;'>" . $row["idActuacio"] . "</td>";
+        echo "<td style='border: 1px solid black;'>" . $row["dataActuacio"] . "</td>";
+        echo "<td style='border: 1px solid black;'>" . $row["descActuacio"] . "</td>";
+        echo "<td style='border: 1px solid black;'>" . $row["visible"] . "</td>";
+        echo "<td style='border: 1px solid black;'>" . $row["temps"] . "</td>";
+        echo "<td style='border: 1px solid black;'>" . $row["incidencia"] . "</td>";
+        echo "</tr>";
+        }
+        ?>
+    </table>
 
-    ?>
+
+
+
+
 </body>
 </html>
