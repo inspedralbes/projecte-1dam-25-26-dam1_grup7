112ec33486c089f282d10e76105b7837e8cdc604
a25unaalvlop <a25unaalvlop@inspedralbes.cat>
2026-04-30 09:12:47 +0200

    crea actuacions funcional

--- Docker_GI3P/php/crear_actuacions.php
+++ Docker_GI3P/php/crear_actuacions.php
@@ -71,12 +71,83 @@ if (isset($_GET['id'])) {
 
 
 
+    <?php
+
+    function crear_actuacions($conn, $id_incidencia){
+    // Obtenir el nom de la casa del formulari
+    $temps = $_POST['temps'];
+
+    $descripcio = $_POST['desc'];
+
+    $visible = $_POST['visible'];
+
+    if (empty($temps)) {
+        echo "<p class='error'>El Temps no pot estar buit.</p>";
+        return;
+    }
+  
+    if (empty($descripcio)) {
+        echo "<p class='error'>La Descripció no pot estar buida.</p>";
+        return;
+    }
+
+
+
+    // Preparar la consulta SQL per inserir una nova casa
+    $sql = "INSERT INTO actuacions (dataActuacio, descActuacio, visible, temps, incidencia) VALUES (NOW(), ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);  //La variable $conn la tenim per haver inclòs el fitxer connexio.php
+    $stmt->bind_param("siii", $descripcio, $visible, $temps, $id_incidencia);
+
+    // Executar la consulta i comprovar si s'ha inserit correctament
+    if ($stmt->execute()) {
+        echo "<p class='info'>Actuació creada amb èxit!</p>";
+    } else {
+        echo "<p class='error'>Error al crear l'actuació: " . htmlspecialchars($stmt->error) . "</p>";
+    }
+
+    // Tancar la declaració i la connexió
+    $stmt->close();
+
+}
+    //farem un select de la taula departaments i recuperarem una matriu de dades
+
+    // Consulta SQL per obtenir totes les files de la taula 'cases'
+    $sql = "SELECT * FROM departament";
+    $result = $conn->query($sql);
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Si el formulari s'ha enviatc (mètode POST), cridem a la funció per crear la casa
+        crear_actuacions($conn, $id_incidencia);
+
+    } else {
+        //Mostrem el formulari per crear una nova casa
+        //Tanquem el php per poder escriure el codi HTML de forma més còmoda.
+        ?>
 
 
 
 
+         <form method="POST" action="crear_actuacions.php?id=<?php echo $id_incidencia; ?>">
+            <fieldset>
+                <label for="description">Descripció:</label>
+                <input type="text" id="desc" name="desc">
+                <br>
+                <label for="temps">Temps total per l'actuació:</label>
+                <input type="number" id="temps" name="temps">
+                <br>
+                <label for="visible">Visible per l'Usuari?</label>
+                <input type="hidden" name="visible" id="no_visible" value="0">
+                <input type="checkbox" id="visible" name="visible" value="1">
+                <input type="submit" value="Crear">
+            </fieldset>
+        </form>
 
 
+        <?php
+        //Tanquem l'else
+    }
+    ?>
 
 
 
