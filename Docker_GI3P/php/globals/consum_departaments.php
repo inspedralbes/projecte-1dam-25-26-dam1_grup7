aab2e81b9c9a47ab83460e6210c3a75331e654bf
a25unaalvlop <a25unaalvlop@inspedralbes.cat>
2026-05-11 11:49:58 +0200

    Fix a consum de departaments

--- Docker_GI3P/php/globals/consum_departaments.php
+++ Docker_GI3P/php/globals/consum_departaments.php
@@ -12,9 +12,31 @@ require_once '../globals/connexio.php';
  * @param mixed $conn
  * @return void
  */
-function veure_consum($conn)
-{
-   
+
+?>
+<!DOCTYPE html>
+<html lang="ca">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consum de Departaments</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+</head>
+
+<header>
+    <a href="../users/usuaris.php" class="btn-back">
+        <span class="arrow">←</span> Tornar
+    </a>
+    <h1>Consum de Departaments</h1>
+</header>
+<hr>
+
+<body class="page-users">
+    <?php
+
     $sql = "SELECT d.nom as nom_dept, COUNT(DISTINCT i.id) as total_incidencies, SUM(a.temps) as total_minuts FROM departament d LEFT JOIN incidencia i ON d.idDept = i.departament LEFT JOIN actuacions a ON i.id = a.incidencia GROUP BY d.idDept HAVING total_incidencies > 0";
 
     $resultat = $conn->query($sql);
@@ -49,29 +71,29 @@ function veure_consum($conn)
 
         ?>
         <script>
-                const colores = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'];
+            const colores = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'];
 
-                new Chart(document.getElementById('chartIncidencies'), {
-                    type: 'doughnut',
-                    data: {
-                        labels: <?php echo $jsonNoms; ?>,
-                        datasets: [{
-                            data: <?php echo $jsonIncidencies; ?>,
-                            backgroundColor: colores
-                        }]
-                    }
-                });
-                new Chart(document.getElementById('chartHores'), {
-                    type: 'doughnut', 
-                    data: {
-                        labels: <?php echo $jsonNoms; ?>,
-                        datasets: [{
-                            label: 'Hores Totals',
-                            data: <?php echo $jsonHores; ?>,
-                            backgroundColor: colores
-                        }]
-                    }
-                });
+            new Chart(document.getElementById('chartIncidencies'), {
+                type: 'doughnut',
+                data: {
+                    labels: <?php echo $jsonNoms; ?>,
+                    datasets: [{
+                        data: <?php echo $jsonIncidencies; ?>,
+                        backgroundColor: colores
+                    }]
+                }
+            });
+            new Chart(document.getElementById('chartHores'), {
+                type: 'doughnut',
+                data: {
+                    labels: <?php echo $jsonNoms; ?>,
+                    datasets: [{
+                        label: 'Hores Totals',
+                        data: <?php echo $jsonHores; ?>,
+                        backgroundColor: colores
+                    }]
+                }
+            });
         </script>
         <?php
     } else {
@@ -79,70 +101,7 @@ function veure_consum($conn)
     }
 
     registrarLog();
-}
 
-
-
-?>
-<!DOCTYPE html>
-<html lang="ca">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consum de Departaments</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-
-</head>
-
-<header>
-    <a href="../users/usuaris.php" class="btn-back">
-        <span class="arrow">←</span> Tornar
-    </a>
-    <h1>Consum de Departaments</h1>
-</header>
-<hr>
-
-<body class="page-users">
-    <?php
-
-    
-    
-    // Consulta SQL per obtenir totes les files de la taula 'cases'
-    $sql = "SELECT * FROM departament";
-    $result = $conn->query($sql);
-
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Si el formulari s'ha enviatc (mètode POST), cridem a la funció per crear la casa
-        veure_consum($conn);
-    } else {
-        //Mostrem el formulari per crear una nova casa
-        //Tanquem el php per poder escriure el codi HTML de forma més còmoda.
-        ?>
-        <main>
-            <form method="POST" action="consum_departaments.php">
-                <fieldset>
-
-                    <label for="departament">Departament</label>
-                    <select name="dep" id="departament">
-                        <?php
-                        while ($row = $result->fetch_assoc()) {
-                            echo "<option value='{$row["idDept"]}'>{$row["nom"]}</option>";
-                        }
-                        ?>
-                    </select>
-
-                    <input type="submit" value="Revisar l'informe">
-                </fieldset>
-            </form>
-        </main>
-
-
-        <?php
-        //Tanquem l'else
-    }
     ?>
 </body>
 <?php include_once "../globals/footer.php"; ?>
